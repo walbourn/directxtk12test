ba134e5de8a2bb700f6793ff1d46bcf517c3002d
Chuck Walbourn <chuckw@windows.microsoft.com>
2016-07-01 18:02:30 -0700

    First basic test

---
+++ D3D12Test/Game.cpp
@@ -0,0 +1,195 @@
+//
+// Game.cpp
+//
+
+#include "pch.h"
+#include "Game.h"
+
+using namespace DirectX;
+using namespace DirectX::SimpleMath;
+
+using Microsoft::WRL::ComPtr;
+
+Game::Game()
+{
+    m_deviceResources = std::make_unique<DX::DeviceResources>();
+    m_deviceResources->RegisterDeviceNotify(this);
+}
+
+// Initialize the Direct3D resources required to run.
+void Game::Initialize(HWND window, int width, int height)
+{
+    m_deviceResources->SetWindow(window, width, height);
+
+    m_deviceResources->CreateDeviceResources();
+    CreateDeviceDependentResources();
+
+    m_deviceResources->CreateWindowSizeDependentResources();
+    CreateWindowSizeDependentResources();
+}
+
+#pragma region Frame Update
+// Executes the basic game loop.
+void Game::Tick()
+{
+    m_timer.Tick([&]()
+    {
+        Update(m_timer);
+    });
+
+    Render();
+}
+
+// Updates the world.
+void Game::Update(DX::StepTimer const& timer)
+{
+    PIXBeginEvent(PIX_COLOR_DEFAULT, L"Update");
+
+    float elapsedTime = float(timer.GetElapsedSeconds());
+    elapsedTime;
+
+    PIXEndEvent();
+}
+#pragma endregion
+
+#pragma region Frame Render
+// Draws the scene.
+void Game::Render()
+{
+    // Don't try to render anything before the first Update.
+    if (m_timer.GetFrameCount() == 0)
+    {
+        return;
+    }
+
+    // Prepare the command list to render a new frame.
+    m_deviceResources->Prepare();
+    Clear();
+
+    auto commandList = m_deviceResources->GetCommandList();
+    PIXBeginEvent(commandList, PIX_COLOR_DEFAULT, L"Render");
+
+    m_effect->Apply(commandList);
+
+    m_batch->Begin(commandList);
+
+    VertexPositionColor v1(Vector3(0.f, 0.5f, 0.5f), Colors::Red);
+    VertexPositionColor v2(Vector3(0.5f, -0.5f, 0.5f), Colors::Green);
+    VertexPositionColor v3(Vector3(-0.5f, -0.5f, 0.5f), Colors::Blue);
+
+    m_batch->DrawTriangle(v1, v2, v3);
+
+    m_batch->End();
+
+    PIXEndEvent(commandList);
+
+    // Show the new frame.
+    PIXBeginEvent(m_deviceResources->GetCommandQueue(), PIX_COLOR_DEFAULT, L"Present");
+    m_deviceResources->Present();
+    m_graphicsMemory->Commit(m_deviceResources->GetCommandQueue());
+    PIXEndEvent(m_deviceResources->GetCommandQueue());
+}
+
+// Helper method to clear the back buffers.
+void Game::Clear()
+{
+    auto commandList = m_deviceResources->GetCommandList();
+    PIXBeginEvent(commandList, PIX_COLOR_DEFAULT, L"Clear");
+
+    // Clear the views.
+    auto rtvDescriptor = m_deviceResources->GetRenderTargetView();
+    auto dsvDescriptor = m_deviceResources->GetDepthStencilView();
+
+    commandList->OMSetRenderTargets(1, &rtvDescriptor, FALSE, &dsvDescriptor);
+    commandList->ClearRenderTargetView(rtvDescriptor, Colors::CornflowerBlue, 0, nullptr);
+    commandList->ClearDepthStencilView(dsvDescriptor, D3D12_CLEAR_FLAG_DEPTH, 1.0f, 0, 0, nullptr);
+
+    // Set the viewport and scissor rect.
+    auto viewport = m_deviceResources->GetScreenViewport();
+    auto scissorRect = m_deviceResources->GetScissorRect();
+    commandList->RSSetViewports(1, &viewport);
+    commandList->RSSetScissorRects(1, &scissorRect);
+
+    PIXEndEvent(commandList);
+}
+#pragma endregion
+
+#pragma region Message Handlers
+// Message handlers
+void Game::OnActivated()
+{
+}
+
+void Game::OnDeactivated()
+{
+}
+
+void Game::OnSuspending()
+{
+}
+
+void Game::OnResuming()
+{
+    m_timer.ResetElapsedTime();
+}
+
+void Game::OnWindowSizeChanged(int width, int height)
+{
+    if (!m_deviceResources->WindowSizeChanged(width, height))
+        return;
+
+    CreateWindowSizeDependentResources();
+}
+
+// Properties
+void Game::GetDefaultSize(int& width, int& height) const
+{
+    width = 800;
+    height = 600;
+}
+#pragma endregion
+
+#pragma region Direct3D Resources
+// These are the resources that depend on the device.
+void Game::CreateDeviceDependentResources()
+{
+    auto device = m_deviceResources->GetD3DDevice();
+
+    m_graphicsMemory = std::make_unique<GraphicsMemory>(device);
+
+    m_batch = std::make_unique<PrimitiveBatch<VertexPositionColor>>(device);
+
+    RenderTargetState rtState(m_deviceResources->GetBackBufferFormat(), m_deviceResources->GetDepthBufferFormat());
+
+    {
+        EffectPipelineStateDescription pd(
+            &VertexPositionColor::InputLayout,
+            &CommonStates::Opaque,
+            &CommonStates::DepthDefault,
+            &CommonStates::CullNone,
+            &rtState);
+
+        m_effect = std::make_unique<BasicEffect>(device, EffectFlags::VertexColor, pd);
+        m_effect->EnableDefaultLighting();
+    }
+}
+
+// Allocate all memory resources that change on a window SizeChanged event.
+void Game::CreateWindowSizeDependentResources()
+{
+}
+
+void Game::OnDeviceLost()
+{
+    m_batch.reset();
+    m_effect.reset();
+    m_graphicsMemory.reset();
+}
+
+void Game::OnDeviceRestored()
+{
+    CreateDeviceDependentResources();
+
+    CreateWindowSizeDependentResources();
+}
+#pragma endregion
