9b50bdb48d82e80869d739bbb31c501688cf9b2f
Chuck Walbourn <walbourn@users.noreply.github.com>
2018-05-31 14:33:19 -0700

    Windows 10 April 2018 Update SDK (17134) (#7)

--- Common/DeviceResourcesPC.cpp
+++ Common/DeviceResourcesPC.cpp
@@ -1,4 +1,4 @@
-﻿//
+//
 // DeviceResources.cpp - A wrapper for the Direct3D 12 device and swapchain
 //
 
@@ -25,6 +25,7 @@ namespace
 };
 
 bool DeviceResources::s_debugForceWarp = false;
+bool DeviceResources::s_debugPreferMinPower = false;
 int DeviceResources::s_debugAdapterOrdinal = -1;
 
 // Constructor for DeviceResources.
@@ -605,7 +606,7 @@ void DeviceResources::GetAdapter(IDXGIAdapter1** ppAdapter)
             for (UINT adapterIndex = 0;
                 DXGI_ERROR_NOT_FOUND != factory6->EnumAdapterByGpuPreference(
                     adapterIndex,
-                    DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE,
+                    s_debugPreferMinPower ? DXGI_GPU_PREFERENCE_MINIMUM_POWER : DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE,
                     IID_PPV_ARGS(adapter.ReleaseAndGetAddressOf()));
                 adapterIndex++)
             {
