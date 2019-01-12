c24ff31378ef2e5f33c92ecb728e429ccf53d886
Chuck Walbourn <chuckw@windows.microsoft.com>
2019-01-11 17:25:07 -0800

    New test

---
+++ PBRModelTest/pch.h
@@ -0,0 +1,115 @@
+//--------------------------------------------------------------------------------------
+// File: pch.h
+//
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+// PARTICULAR PURPOSE.
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// http://go.microsoft.com/fwlink/?LinkID=615561
+//--------------------------------------------------------------------------------------
+
+#pragma once
+
+// Use the C++ standard templated min/max
+#define NOMINMAX
+
+#include <winapifamily.h>
+
+#if defined(_XBOX_ONE) && defined(_TITLE)
+#include <xdk.h>
+#elif !defined(WINAPI_FAMILY) || (WINAPI_FAMILY == WINAPI_FAMILY_DESKTOP_APP) 
+#include <WinSDKVer.h>
+#define _WIN32_WINNT 0x0A00
+#include <SDKDDKVer.h>
+
+#pragma warning(push)
+#pragma warning(disable : 4005)
+#define NOMINMAX
+#define NODRAWTEXT
+#define NOGDI
+#define NOBITMAP
+#define NOMCX
+#define NOSERVICE
+#define NOHELP
+#define WIN32_LEAN_AND_MEAN
+#pragma warning(pop)
+
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
+#include <windows.h>
+#endif
+
+#include <wrl.h>
+
+#if defined(_XBOX_ONE) && defined(_TITLE)
+#include <d3d12_x.h>
+#include <d3dx12_x.h>
+#else
+#include <d3d12.h>
+
+#if defined(NTDDI_WIN10_RS2)
+#include <dxgi1_6.h>
+#else
+#include <dxgi1_5.h>
+#endif
+
+#ifdef _DEBUG
+#include <dxgidebug.h>
+#endif
+
+#include "d3dx12.h"
+#endif
+
+#include <DirectXMath.h>
+#include <DirectXColors.h>
+
+#include <algorithm>
+#include <exception>
+#include <memory>
+#include <stdexcept>
+
+#include <stdio.h>
+#include <pix.h>
+
+#include "DescriptorHeap.h"
+#include "GamePad.h"
+#include "GraphicsMemory.h"
+#include "Keyboard.h"
+#include "Mouse.h"
+#include "PostProcess.h"
+#include "RenderTargetState.h"
+#include "SimpleMath.h"
+
+namespace DX
+{
+    // Helper class for COM exceptions
+    class com_exception : public std::exception
+    {
+    public:
+        com_exception(HRESULT hr) : result(hr) {}
+
+        virtual const char* what() const override
+        {
+            static char s_str[64] = {};
+            sprintf_s(s_str, "Failure with HRESULT of %08X", static_cast<int>(result));
+            return s_str;
+        }
+
+    private:
+        HRESULT result;
+    };
+
+    // Helper utility converts D3D API failures into exceptions.
+    inline void ThrowIfFailed(HRESULT hr)
+    {
+        if (FAILED(hr))
+        {
+            throw com_exception(hr);
+        }
+    }
+}
